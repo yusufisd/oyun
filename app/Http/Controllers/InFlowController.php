9c72c04192a76d262af9a682d81befe675e90c6e
yusuf ishak damar <ysfishak14@gmail.com>
2025-11-15 13:41:21 +0300

    inflow & outflow backend revision

--- app/Http/Controllers/InFlowController.php
+++ app/Http/Controllers/InFlowController.php
@@ -4,27 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Transaction;
-
+use Illuminate\Support\Facades\Auth;
 class InFlowController extends Controller
 {
     public function store(Request $request)
     {
-        $request->validate([
-            'amount' => 'required|numeric',
-            'payment_method' => 'required|in:bank_transfer,credit_card,mobile_money',
-            'datetime' => 'required|date',
-            'type' => 'required|in:inflow,outflow',
-        ]);
-
-        $transaction = Transaction::create([
-            'amount' => $request->amount,
-            'payment_method' => $request->payment_method,
-            'datetime' => $request->datetime,
-            'type' => $request->type,
-        ]);
-
-      
-
-        return redirect()->route('transactions.index');
+        try{
+            $request->validate([
+                'amount' => 'required|numeric',
+                'payment_method' => 'required|in:bank_transfer,credit_card,mobile_money',
+                'datetime' => 'required|date',
+            ]);
+    
+            $transaction = Transaction::create([
+                'user_id' => Auth::user()->id ,
+                'amount' => $request->amount,
+                'payment_method' => $request->payment_method,
+                'datetime' => $request->datetime,
+                'type' => 'inflow',
+            ]);
+            if($transaction){
+                return response()->json(['message' => 'Transaction created successfully'], 201);
+            } else {
+                return response()->json(['message' => 'Transaction creation failed'], 500);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Validation failed', 'errors' => $e->getMessage()], 422);
+        }
     }
 }
--- app/Http/Controllers/OutFlowController.php
+++ app/Http/Controllers/OutFlowController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Transaction;
+use Illuminate\Support\Facades\Auth;
 class OutFlowController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        try{
+            $request->validate([
+                'amount' => 'required|numeric',
+                'payment_method' => 'required|in:bank_transfer,credit_card,mobile_money',
+                'datetime' => 'required|date',
+            ]);
+            $transaction = Transaction::create([
+                'user_id' => Auth::user()->id ,
+                'amount' => $request->amount,
+                'payment_method' => $request->payment_method,
+                'datetime' => $request->datetime,
+                'type' => 'outflow',
+            ]);
+            if($transaction){
+                return response()->json(['message' => 'Transaction created successfully'], 201);
+            } else {
+                return response()->json(['message' => 'Transaction creation failed'], 500);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Validation failed', 'errors' => $e->getMessage()], 422);
+        }
+    }
 }
